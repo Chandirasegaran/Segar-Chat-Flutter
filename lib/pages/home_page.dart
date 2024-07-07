8643b86387c4538c9238b16269c4ca77bd8a6c5c
Chandirasegaran <chandirasegaransegar@gmail.com>
2024-07-07 12:53:12 +0530

    ChatTile - Listing of Users Completed --Segar --07-07-2024 --12:53

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:segarchat/models/user_profile.dart';
 import 'package:segarchat/services/alert_services.dart';
 import 'package:segarchat/services/auth_service.dart';
+import 'package:segarchat/services/database_service.dart';
 import 'package:segarchat/services/navigation_service.dart';
+import 'package:segarchat/widgets/chat_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,13 +19,14 @@ class _HomePageState extends State<HomePage> {
   late AuthService _authService;
   late NavigationService _navigationService;
   late AlertServices _alertServices;
-
+  late DatabaseService _databaseService;
   @override
   void initState() {
     super.initState();
     _authService = _getIt.get<AuthService>();
     _navigationService = _getIt.get<NavigationService>();
     _alertServices = _getIt.get<AlertServices>();
+    _databaseService = _getIt.get<DatabaseService>();
   }
 
   @override
@@ -47,9 +51,49 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text("Hello Segar"),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 15.0,
+          vertical: 20.0,
+        ),
+        child: _chatList(),
       ),
     );
+  }
+
+  Widget _chatList() {
+    return StreamBuilder(
+        stream: _databaseService.getUserProfile(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Unable to load data."),
+            );
+          }
+          if (snapshot.hasData && snapshot.data != null) {
+            final users = snapshot.data!.docs;
+            return ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  UserProfile user = users[index].data();
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10.0,
+                    ),
+                    child: ChatTile(userProfile: user, onTap: () {}),
+                  );
+                });
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 }
--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,10 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_it/get_it.dart';
 import 'package:segarchat/models/user_profile.dart';
+import 'package:segarchat/services/auth_service.dart';
 
 class DatabaseService {
+  final GetIt _getIt = GetIt.instance;
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  late AuthService _authService;
   CollectionReference? _userCollection;
   DatabaseService() {
+    _authService = _getIt.get<AuthService>();
     _setupCollectionReferences();
   }
 
@@ -19,5 +24,11 @@ class DatabaseService {
 
   Future<void> createUserProfile({required UserProfile userProfile}) async {
     await _userCollection?.doc(userProfile.uid).set(userProfile);
+  }
+
+  Stream<QuerySnapshot<UserProfile>> getUserProfile() {
+    return _userCollection
+        ?.where("uid", isNotEqualTo: _authService.user!.uid)
+        .snapshots() as Stream<QuerySnapshot<UserProfile>>;
   }
 }
